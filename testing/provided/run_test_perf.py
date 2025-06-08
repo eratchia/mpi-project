6594e5c5cb0c85c2c380bf48f060c8410076d501
eratchia <eratchias@gmail.com>
2025-06-08 20:28:39 +0200

    add perf testing

---
+++ testing/provided/run_test_perf.py
@@ -0,0 +1,44 @@
+import argparse
+from pathlib import Path
+import subprocess
+import filecmp
+
+def run_test(break_on_fail, local, path_to_test):
+    Path("outputs").mkdir(parents=True, exist_ok=True)
+    solution = Path("aw429669")
+    make = subprocess.run("make", cwd=solution.name, capture_output=True, timeout=300)
+    if make.returncode != 0:
+        print(f"{solution.name}: FAILED (make)")
+        if break_on_fail:
+            print(make.stdout)
+            exit(1)
+        exit(1)
+    print(f"Solution: {solution.name}")
+
+    test = Path(path_to_test)
+    workers = int(test.name[test.name.rfind("_") + 1:])
+    # Remove old outputs
+    for f in Path("outputs").iterdir():
+        f.unlink()
+    if local:
+        command = "mpiexec"
+    else:
+        command = "srun"
+    execution = subprocess.run([command, "-n", str(workers), "./test_command.sh", solution.name, test.name], capture_output=True, timeout=300)
+    if execution.returncode != 0:
+        print(f"    {test.name}: FAILED (srun)")
+        if break_on_fail:
+            print(f"{execution.stdout=}")
+            print(f"{execution.stderr=}")
+            exit(1)
+        exit(1)
+    print(f"    {test.name}: PASSED")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Test runner')
+    parser.add_argument('-b', '--breakonfail', action='store_true', help='break and print stdout on fail')
+    parser.add_argument('-l', '--local', action='store_true', help='run tests locally (without slurm)')
+    parser.add_argument('path_to_test', help='run tests locally (without slurm)')
+
+    args = parser.parse_args()
+    run_test(args.breakonfail, args.local, args.path_to_test)
